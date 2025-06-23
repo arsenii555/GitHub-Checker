1b67c4478c79aa8b23e3214986fd7ece2babf9be
eremanikita <eremanikita@gmail.com>
2025-06-23 20:04:41 +0300

    /untrack endpoint added to scrapper

--- scrapper/main.py
+++ scrapper/main.py
@@ -15,3 +15,12 @@ async def track_link(link: Link):
     link = link.url
     users_links[user].add(link)
     return {"message": "link added", "success": True}
+
+
+@app.post("/untrack")
+async def untrack_link(link: Link):
+    """Untrack link endpoint."""
+    user = link.user_id
+    link = link.url
+    users_links[user].discard(link)
+    return {"message": "link removed", "success": True}
