fffbf75c499416c9244bd3bf2a5e22a440ee36f7
eremanikita <eremanikita@gmail.com>
2025-06-23 17:34:56 +0300

    http client added

---
+++ bot/client.py
@@ -0,0 +1,33 @@
+"""HTTP client."""
+import aiohttp
+
+
+class HTTPClient:
+    """HTTP client."""
+
+    def __init__(self, base_url: str):
+        """Initialize HTTP client."""
+        self.base_url = base_url
+        self.session = None
+
+    async def start(self):
+        """Create session."""
+        self.session = aiohttp.ClientSession()
+
+    async def close(self):
+        """Close session."""
+        if self.session:
+            self.session.close()
+
+    async def _request(self, method: str, endpoint: str, **kwargs):
+        """Send request and return response."""
+        url = f"{self.base_url}/{endpoint.lstrip('/')}"
+        async with self.session.request(method, url, **kwargs) as response:
+            response.raise_for_status()
+            return await response.json() if response.status != 204 else None
+
+    async def track_link(self, user_id: int, link: str) -> bool:
+        """Send track link request."""
+        payload = {"url": link, "user_id": user_id}
+        response = await self._request("POST", "/track", json=payload)
+        return response is not None and response.get("success", False)
--- bot/handlers/track.py
+++ bot/handlers/track.py
@@ -1,14 +1,20 @@
 """Track handler."""
 from telethon.events import NewMessage
+from bot.client import HTTPClient
 
 
-async def track_cmd_handler(event: NewMessage.Event, ) -> None:
+async def track_cmd_handler(http_client: HTTPClient, event: NewMessage.Event, ) -> None:
     """Handle /track command."""
     resp_msg: str
+    user_id = event.chat_id
     msg_text = event.message.text
     match msg_text.split():
         case ["/track", link]:
-            resp_msg = f"Ссылка\n{link}\nуспешно добавлена для отслеживания ✅"
+            success = await http_client.track_link(user_id=user_id, link=link)
+            if success:
+                resp_msg = f"Ссылка\n{link}\nуспешно добавлена для отслеживания ✅"
+            else:
+                resp_msg = "Произошла ошибка при добавлении ссылки ❌"
         case _:
             resp_msg = "Отправьте сообщение вида:\n/track <link>"
     await event.client.send_message(
